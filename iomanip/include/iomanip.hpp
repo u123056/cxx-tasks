b9ac1d2c003b629007a1d9268a10250fc31d3404
u123056 <36473380+u123056@users.noreply.github.com>
2018-10-28 17:42:12 +0300

    Update iomanip.hpp

--- iomanip/include/iomanip.hpp
+++ iomanip/include/iomanip.hpp
@@ -10,15 +10,49 @@
 #include <iostream>
 
 //{ endm manipulator
-
+std::ostream& endm(std::ostream& os)
+{
+    os << "[eol]\n";
+    return os;
+}
 //}
-
 //{ squares manipulator
-
+struct squares_proxy {
+public:
+    explicit squares_proxy(std::ostream& os):os(os){}
+    template<typename T>
+    friend std::ostream & operator<<(squares_proxy const& q, T const& word) {
+        return q.os << "[" << word << "]";
+    }
+private:
+    std::ostream & os;
+};
+struct squares_creator { } squares;
+squares_proxy operator<<(std::ostream& os, squares_creator) {
+    return squares_proxy(os);
+}
 //}
-
 //{ add manipulator
-
+struct add_proxy_next {
+    explicit add_proxy_next(std::ostream & os, long const& num): os(os), previous_num(num) {}
+    std::ostream& operator<<(long const& current_num) {
+        return os << previous_num + current_num;
+    }
+private:
+    std::ostream & os;
+    long previous_num;
+};
+struct add_proxy {
+    explicit add_proxy(std::ostream & os): os(os) {}
+    struct add_proxy_next operator<<(long const& current_num) {
+        return add_proxy_next(os, current_num);
+    }
+private:
+    std::ostream & os;
+};
+struct add_creator { } add;
+add_proxy operator<<(std::ostream & os, add_creator) {
+    return add_proxy(os);
+}
 //}
-
 #endif // __IOMANIP_HPP__
