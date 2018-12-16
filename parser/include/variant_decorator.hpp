877391cbccf1916c0ad82dda967ae54cea48888f
Georgy Srebnitski <srebgeorge@gmail.com>
2018-12-16 15:36:38 +0300

    Исправление замечаний

--- parser/include/variant_decorator.hpp
+++ parser/include/variant_decorator.hpp
@@ -22,6 +22,9 @@ struct variant_decorator: public x3::variant<U...>
     using x3::variant<U...>::operator=;
     using x3::variant<U...>::variant;
 
+    variant_decorator(variant_decorator const&)=default;
+    variant_decorator& operator=(variant_decorator const&)=default;
+
     //? Do I need a ``const``-version?
     template<class T>
     T& as()
