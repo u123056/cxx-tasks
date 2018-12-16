02e3f6d810d23a03bba9f931d92d58858d7d41bd
Georgy Srebnitski <srebgeorge@gmail.com>
2018-12-16 16:03:46 +0300

    Исправление замечаний

--- variant/include/variant.hpp
+++ variant/include/variant.hpp
@@ -7,7 +7,6 @@
 #ifndef __VARIANT_HPP__
 #define __VARIANT_HPP__
 
-#include <any>
 #include <map>
 #include <memory>
 #include <variant>
@@ -18,28 +17,28 @@
 //{ number
 struct number : public std::variant<int, float>
 {
-	using std::variant<int, float>::variant;
+	using variant::variant;
 };
 //}
 
 //{ array
 struct array : public std::vector<number>
 {
-    using std::vector<number>::vector;
+    using vector::vector;
 };
 //}
 
 //{ recursive_array
 struct recursive_array : public std::vector<std::variant<number, std::shared_ptr<recursive_array>>>
 {
-    using std::vector<std::variant<number, std::shared_ptr<recursive_array>>>::vector;
+    using vector::vector;
 };
 //}
 
 //{ recursive_array2
 struct recursive_array2 : public std::vector<std::variant<number, boost::recursive_wrapper<recursive_array2>>>
 {
-    using std::vector<std::variant<number, boost::recursive_wrapper<recursive_array2>>>::vector;
+    using vector::vector;
 };
 //}
 
@@ -50,12 +49,12 @@ struct variant_decorator : public std::variant<Args...>
 	using std::variant<Args...>::variant;
 
 	template <typename T>
-    const T& as() const {
-		return const_cast<T>(this->as<T>());
+    T& as() {
+		return const_cast<T&>(const_cast<const variant_decorator *>(this)->as<T>());
 	}
 
 	template <typename T>
-	T& as() {
+	const T& as() const {
 		if constexpr (!std::disjunction_v<std::is_same<T, Args>...>) {
 			return std::get<boost::recursive_wrapper<T>>(*this).get();
 		} else {
@@ -68,7 +67,7 @@ struct variant_decorator : public std::variant<Args...>
 //{ recursive_map
 struct recursive_map : public std::map<std::string, variant_decorator< int, bool, std::string,  boost::recursive_wrapper<recursive_map>>>
 {
-	using std::map<std::string, variant_decorator< int, bool, std::string,  boost::recursive_wrapper< recursive_map >>>::map;
+	using map::map;
 };
 //}
 
