d0617c43f0b96e1051e1198ab026b55f264f08ca
Georgy Srebnitski <srebgeorge@gmail.com>
2018-12-16 15:44:27 +0300

    Исправление замечаний

--- parser/include/csv.hpp
+++ parser/include/csv.hpp
@@ -70,7 +70,7 @@ namespace literals
         //{ declare ``_csv`` literal
         types::csv::csv operator "" _csv(const char* s, long unsigned int size)
         {
-            return parser::load_from_string<types::csv::csv>(s, parser::csv::csv);
+            return parser::load_from_string<types::csv::csv>(std::string(s, size), parser::csv::csv);
         }
         //}
     }
